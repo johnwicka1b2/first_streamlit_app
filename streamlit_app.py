49d5e43ddec4987687789c7556b58fa2b06bf1b0
Kedar Krishnanand Kini <121103748+johnwicka1b2@users.noreply.github.com>
2022-12-22 17:05:21 +0530

    Update streamlit_app.py
    adding try except and if else statement so it works every time we enter a new value is entered otherwise throw an error

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,19 +26,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    # import requests
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    # streamlit.text(fruityvice_response.json())
+    # take the json version of the response and normalize it
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # output it to the screen as table
+    streamlit.dataframe(fruityvice_normalized)
+except URLERROR as e:
+  streamlit.error()
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-# import requests
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# streamlit.text(fruityvice_response.json())
-
-# take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it to the screen as table
-streamlit.dataframe(fruityvice_normalized)
 
 # import snowflake.connector
 
