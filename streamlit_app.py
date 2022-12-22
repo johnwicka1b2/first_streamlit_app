e0a99779e4691234c9a823e3467795ed273d5295
Kedar Krishnanand Kini <121103748+johnwicka1b2@users.noreply.github.com>
2022-12-22 17:11:05 +0530

    Update streamlit_app.py
    Adding a function for getting fruity vice data (move code so it is more reusable)

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,19 +25,23 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+def get_fruityvice_data(this_fruit_choice):
+  # import requests
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+  # streamlit.text(fruityvice_response.json())
+  # take the json version of the response and normalize it
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    # import requests
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    # streamlit.text(fruityvice_response.json())
-    # take the json version of the response and normalize it
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    back_from_function = get_fruityvice_data(fruit_choice)
     # output it to the screen as table
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 
